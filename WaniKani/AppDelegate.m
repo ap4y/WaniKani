bad195f482acee4718a8dda6772cf69fd6dc5ee7
ap4y <lod@pisem.net>
2013-02-13 14:31:46 +1300

    storyboard added

--- WaniKani/AppDelegate.m
+++ WaniKani/AppDelegate.m
@@ -11,10 +11,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
