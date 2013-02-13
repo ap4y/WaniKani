f398aa5e005bc9770641041f26d43439ec1c290c
ap4y <lod@pisem.net>
2013-02-13 15:20:34 +1300

    kanji view controller added

---
+++ WaniKani/ViewControllers/WKKanjiViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKKanjiViewController.h
+//  WaniKani
+//
+//  Created by Arthur Evstifeev on 13/02/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKKanjiViewController : UITableViewController
+
+@end
---
+++ WaniKani/ViewControllers/WKKanjiViewController.m
@@ -0,0 +1,56 @@
+//
+//  WKKanjiViewController.m
+//  WaniKani
+//
+//  Created by Arthur Evstifeev on 13/02/13.
+//
+//
+
+#import "WKKanjiViewController.h"
+
+@interface WKKanjiViewController ()
+@property (strong, nonatomic) IBOutlet UITableView *kanjiTableView;
+
+@property (strong, nonatomic) NSArray *kanji;
+@end
+
+@implementation WKKanjiViewController
+
+static NSString * const kKanjiCellIdentifier = @"WKKanjiCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [WKKanji fetchKanjiWithSuccess:^(NSArray *kanji) {
+        
+        self.kanji = kanji;
+        [_kanjiTableView reloadData];
+        
+    } failure:^(NSError *error) {
+        
+        /**
+         TODO: Add error handling
+         */
+        NSLog(@"%@", error);
+    }];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [_kanji count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kKanjiCellIdentifier];
+    
+    WKKanji *kanjiItem  = [_kanji objectAtIndex:indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ - %@,%@ - %@",
+                           kanjiItem.character, kanjiItem.onyomi,
+                           kanjiItem.kunyomi, kanjiItem.meaning];
+    
+    return cell;
+}
+
+@end
