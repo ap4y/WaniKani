1bfa41b10d448b02901880263acb555483b8d2a1
ap4y <lod@pisem.net>
2013-02-16 13:06:57 +1300

    url caching and network indicator added

--- WaniKani/AppDelegate.m
+++ WaniKani/AppDelegate.m
@@ -7,11 +7,18 @@
 //
 
 #import "AppDelegate.h"
+#import "AFNetworkActivityIndicatorManager.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
+    NSURLCache *urlCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024
+                                                         diskCapacity:20 * 1024 *1024
+                                                             diskPath:@"WKURLCache"];
+    [NSURLCache setSharedURLCache:urlCache];
+    
     return YES;
 }
 
