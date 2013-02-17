90a30f1d24e23de4af9e07b5ca1795333199f7b1
ap4y <lod@pisem.net>
2013-02-17 18:29:00 +1300

    radicals collection view cell added

---
+++ WaniKani/Views/WKRadicalViewCell.h
@@ -0,0 +1,14 @@
+//
+//  WKRadicalCell.h
+//  WaniKani
+//
+//  Created by Arthur Evstifeev on 17/02/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class WKRadical;
+@interface WKRadicalViewCell : UICollectionViewCell
+- (void)setRadical:(WKRadical *)radical;
+@end
---
+++ WaniKani/Views/WKRadicalViewCell.m
@@ -0,0 +1,76 @@
+//
+//  WKRadicalCell.m
+//  WaniKani
+//
+//  Created by Arthur Evstifeev on 17/02/13.
+//
+//
+
+#import "WKRadicalViewCell.h"
+#import <QuartzCore/QuartzCore.h>
+#import "UIImageView+AFNetworking.h"
+
+#import "WKRadical.h"
+
+@interface WKRadicalViewCell ()
+@property (strong, nonatomic) IBOutlet UICollectionViewCell *cellView;
+@property (weak, nonatomic) IBOutlet UILabel *characterLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *characterImageView;
+@property (weak, nonatomic) IBOutlet UILabel *meaningLabel;
+
+@end
+
+@implementation WKRadicalViewCell
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+    
+    [self createViewFromNib];
+    
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (!self) return nil;
+    
+    [self createViewFromNib];
+    
+    return self;
+}
+
+- (void)setRadical:(WKRadical *)radical {
+    
+    if (radical.character) {
+        
+        _characterLabel.text        = radical.character;
+        _characterImageView.image   = nil;
+        
+    } else {
+        
+        _characterLabel.text        = nil;
+        [_characterImageView setImageWithURL:[NSURL URLWithString:radical.image]];
+    }
+    _meaningLabel.text      = [[radical.meaning componentsSeparatedByString:@","] objectAtIndex:0];
+}
+
+#pragma mark - private
+
+- (void)createViewFromNib {
+    
+    [[NSBundle mainBundle] loadNibNamed:@"WKRadicalViewCell" owner:self options:nil];
+    [self addSubview:_cellView];
+    
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    
+    gradientLayer.startPoint    = CGPointMake(0.5f, 0.0f);
+    gradientLayer.endPoint      = CGPointMake(0.5f, 1.0f);
+    gradientLayer.frame         = self.bounds;
+    gradientLayer.colors        = @[ (id)[RGBA(0.0, 170.0, 255.0, 1.0) CGColor],
+                                     (id)[RGBA(0.0, 147.0, 221.0, 1.0) CGColor] ];
+    
+    [self.layer insertSublayer:gradientLayer atIndex:0];
+}
+
+@end
