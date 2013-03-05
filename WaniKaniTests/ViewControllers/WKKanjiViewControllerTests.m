0f0cb17e444475841bb587da36834f3b83bb61dd
ap4y <lod@pisem.net>
2013-03-05 18:49:35 +1300

    vocab, kanji, radical controllers tests updated

--- WaniKaniTests/ViewControllers/WKKanjiViewControllerTests.m
+++ WaniKaniTests/ViewControllers/WKKanjiViewControllerTests.m
@@ -10,15 +10,8 @@
 #import "WKKanjiViewController.h"
 #import "WKKanji.h"
 
-#import "WKTestHelpers.h"
-
-@interface WKKanjiViewController ()
-@property (strong, nonatomic) NSArray *kanji;
-@end
-
 @interface WKKanjiViewControllerTests ()
 @property (strong, nonatomic) WKKanjiViewController *subject;
-@property (strong, nonatomic) WKKanji *kanjiItem;
 @end
 
 @implementation WKKanjiViewControllerTests
@@ -26,42 +19,15 @@
 - (void)setUp {
     [super setUp];
     
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Storyboard" bundle:[NSBundle mainBundle]];
-    self.subject = [storyboard instantiateViewControllerWithIdentifier:@"WKKanjiViewControllerTests"];
-    [_subject loadView];
-    
-    NSArray *kanji = [WKKanji requestResult:[WKKanji all] managedObjectContext:mainThreadContext()];
-    [kanji enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        [mainThreadContext() deleteObject:obj];
-    }];
-    
-    self.kanjiItem = [NSEntityDescription insertNewObjectForEntityForName:@"WKKanji"
-                                                   inManagedObjectContext:mainThreadContext()];
-    _kanjiItem.id         = @"seven";
-    _kanjiItem.character  = @"七";    
+    self.subject = [[WKKanjiViewController alloc] init];
 }
 
-- (void)tearDown {
-    [OHHTTPStubs removeAllRequestHandlers];
+- (void)testControllerConfiguration {
     
-    [super tearDown];
-}
-
-- (void)testFetchKanji {
-    [OHHTTPStubs addRequestHandler:^OHHTTPStubsResponse *(NSURLRequest *request, BOOL onlyCheck) {
-        return [OHHTTPStubsResponse responseWithFile:@"kanji.json"
-                                         contentType:@"application/json"
-                                        responseTime:0.0];
-    }];
-    
-    [AETestHelpers runAsyncTest:^(BOOL *endCondition) {
-        
-        [_subject viewDidLoad];
-        STAssertEqualObjects(@[ _kanjiItem ], _subject.kanji, nil);
-        
-    } interval:0.1];
-    
-    STAssertEquals(18U, [_subject.kanji count], nil);
+    STAssertEqualObjects([WKKanji class],           [_subject itemClass],                                   nil);
+    STAssertEqualObjects(@"WKKanjiCell",            [_subject collectionItemCellViewIdentifier],            nil);
+    STAssertEqualObjects(@"WKKanjiSuppView",        [_subject collectionItemSupplementaryViewIdentifier],   nil);
+    STAssertEqualObjects(@"WKKanjiDetailsSegue",    [_subject detailsSegueIdentifier],                      nil);
 }
 
 @end
--- WaniKaniTests/ViewControllers/WKRadicalsViewControllerTests.m
+++ WaniKaniTests/ViewControllers/WKRadicalsViewControllerTests.m
@@ -10,15 +10,8 @@
 #import "WKRadicalsViewController.h"
 #import "WKRadical.h"
 
-#import "WKTestHelpers.h"
-
-@interface WKRadicalsViewController ()
-@property (strong, nonatomic) NSArray *radicals;
-@end
-
 @interface WKRadicalsViewControllerTests ()
 @property (strong, nonatomic) WKRadicalsViewController *subject;
-@property (strong, nonatomic) WKRadical *radical;
 @end
 
 @implementation WKRadicalsViewControllerTests
@@ -26,42 +19,14 @@
 - (void)setUp {
     [super setUp];
     
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Storyboard" bundle:[NSBundle mainBundle]];
-    self.subject = [storyboard instantiateViewControllerWithIdentifier:@"WKRadicalsViewController"];
-    [_subject loadView];
-    
-    NSArray *radicals = [WKRadical requestResult:[WKRadical all] managedObjectContext:mainThreadContext()];
-    [radicals enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        [mainThreadContext() deleteObject:obj];
-    }];
-    
-    self.radical  = [NSEntityDescription insertNewObjectForEntityForName:@"WKRadical"
-                                                  inManagedObjectContext:mainThreadContext()];
-    _radical.id         = @"ground";
-    _radical.character  = @"一";
-}
+    self.subject = [[WKRadicalsViewController alloc] init];}
 
-- (void)tearDown {
-    [OHHTTPStubs removeAllRequestHandlers];
+- (void)testControllerConfiguration {
     
-    [super tearDown];
-}
-
-- (void)testFetchRadicals {
-    [OHHTTPStubs addRequestHandler:^OHHTTPStubsResponse *(NSURLRequest *request, BOOL onlyCheck) {
-        return [OHHTTPStubsResponse responseWithFile:@"radicals.json"
-                                         contentType:@"application/json"
-                                        responseTime:0.0];        
-    }];
-    
-    [AETestHelpers runAsyncTest:^(BOOL *endCondition) {
-        
-        [_subject viewDidLoad];
-        STAssertEqualObjects(@[ _radical ], _subject.radicals, nil);
-        
-    } interval:0.1];
-    
-    STAssertEquals(27U, [_subject.radicals count], nil);
+    STAssertEqualObjects([WKRadical class],         [_subject itemClass],                                   nil);
+    STAssertEqualObjects(@"WKRadicalsCell",         [_subject collectionItemCellViewIdentifier],            nil);
+    STAssertEqualObjects(@"WKRadicalsSuppView",     [_subject collectionItemSupplementaryViewIdentifier],   nil);
+    STAssertEqualObjects(@"WKRadicalDetailsSegue",  [_subject detailsSegueIdentifier],                      nil);
 }
 
 @end
