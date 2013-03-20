4037307c0d299719011af6c2d5566283ebd64b68
ap4y <lod@pisem.net>
2013-03-20 22:07:41 +1300

    login state tracking via progress HUD added.

--- WaniKani/ViewControllers/WKLoginViewController.m
+++ WaniKani/ViewControllers/WKLoginViewController.m
@@ -11,6 +11,7 @@
 #import "WKCustomization.h"
 #import "WKHijackHTTPClient.h"
 
+#import "SVProgressHUD.h"
 #import "AEAlert.h"
 
 @interface WKLoginViewController () <UITextFieldDelegate>
@@ -57,14 +58,18 @@ static NSString * const kRadicalsSegueIdentifier = @"WKRadicalsSegue";
 
     WKHijackHTTPClient *hijackClient = [WKHijackHTTPClient sharedClient];
 
+    [SVProgressHUD showProgress:0.0f status:NSLocalizedString(@"Processing...", nil)];
     [hijackClient loginWithUsername:_userNameTextField.text
                            password:_passwordTextField.text
                            remember:YES success:^(NSString *apiKey) {
                               
+                               [SVProgressHUD showProgress:0.5f
+                                                    status:NSLocalizedString(@"Requesting API key...", nil)];
                                [self checkApiKey:apiKey];
                                
                            } failure:^(NSError *error) {
                                
+                               [SVProgressHUD dismiss];
                                NSString *errorMessage = NSLocalizedString(@"Unable to login. Please check input.", nil);
                                [AEAlert composeAlertViewWithTitle:NSLocalizedString(@"Error", nil)
                                                        andMessage:errorMessage];
@@ -80,11 +85,13 @@ static NSString * const kRadicalsSegueIdentifier = @"WKRadicalsSegue";
     client.userKey          = apiKey;
     [client pingRequestWithSuccess:^(id responseObject) {
         
+        [SVProgressHUD showSuccessWithStatus:NSLocalizedString(@"Success!", nil)];
         client.gravatarId   = [responseObject valueForKeyPath:@"user_information.gravatar"];
         [self performSegueWithIdentifier:kRadicalsSegueIdentifier sender:self];
         
     } failure:^(NSError *error) {
         
+        [SVProgressHUD show];
         [AEAlert composeAlertViewWithTitle:NSLocalizedString(@"Error", nil)
                                 andMessage:NSLocalizedString(@"Unable to connect. Please check your API key.", nil)];
         client.userKey = nil;
