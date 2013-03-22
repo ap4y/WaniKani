d4fbe095c622dc60a993e83138ef8e28bf0e402b
ap4y <lod@pisem.net>
2013-03-22 18:52:35 +1300

    global sync progress indication added through custom tab bar controller

--- WaniKani/ViewControllers/WKTabBarController.m
+++ WaniKani/ViewControllers/WKTabBarController.m
@@ -9,22 +9,71 @@
 #import "WKTabBarController.h"
 #import "WKStatsManager.h"
 #import "AEAlert.h"
+#import "WKSyncManager.h"
 
 @interface WKTabBarController () <UITabBarControllerDelegate>
+@property (strong, nonatomic) IBOutlet UIView *progressView;
 
 @end
 
 @implementation WKTabBarController
 
-static const NSUInteger kReviewsItemIndex = 2;
+static const NSUInteger kReviewsItemIndex       = 2;
+static const NSTimeInterval kAnimationDuration  = 0.2;
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (!self) return nil;
     
     self.delegate = self;
-        
+
+    [[NSNotificationCenter defaultCenter] addObserverForName:WKSyncMangerDidStartNotification
+                                                      object:[WKSyncManager sharedManager]
+                                                       queue:[NSOperationQueue mainQueue]
+                                                  usingBlock:^(NSNotification *note) {
+                                                      
+                                                      [self setProgressViewHidden:NO];
+                                                  }];
+    [[NSNotificationCenter defaultCenter] addObserverForName:WKSyncMangerDidSyncNotification
+                                                      object:[WKSyncManager sharedManager]
+                                                       queue:[NSOperationQueue mainQueue]
+                                                  usingBlock:^(NSNotification *note) {
+                                                      
+                                                      [self setProgressViewHidden:YES];
+                                                  }];
+    [[NSNotificationCenter defaultCenter] addObserverForName:WKSyncMangerDidFailNotification
+                                                      object:[WKSyncManager sharedManager]
+                                                       queue:[NSOperationQueue mainQueue]
+                                                  usingBlock:^(NSNotification *note) {
+                                                      
+                                                      [self setProgressViewHidden:YES];
+                                                  }];
+    
     return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [[NSBundle mainBundle] loadNibNamed:@"WKSyncProgressView" owner:self options:nil];
+    [self.view insertSubview:_progressView belowSubview:self.tabBar];
+
+    CGRect progressViewFrame    = _progressView.frame;
+    progressViewFrame.origin.y  = self.view.bounds.size.height - progressViewFrame.size.height - 49.0f;
+    _progressView.frame         = progressViewFrame;    
+}
+
+#pragma mark - private
+
+- (void)setProgressViewHidden:(BOOL)hidden {
+ 
+    [UIView animateWithDuration:kAnimationDuration animations:^{
+       
+        CGRect progressViewFrame    = _progressView.frame;
+        CGFloat gapHeight           = ( hidden ? 0.0f : progressViewFrame.size.height );
+        progressViewFrame.origin.y  = self.view.bounds.size.height - 49.0f - gapHeight;
+        _progressView.frame         = progressViewFrame;
+    }];
 }
 
 #pragma mark - UITabBarControllerDelegate
@@ -54,4 +103,8 @@ static const NSUInteger kReviewsItemIndex = 2;
     return YES;
 }
 
+- (void)viewDidUnload {
+    [self setProgressView:nil];
+    [super viewDidUnload];
+}
 @end
